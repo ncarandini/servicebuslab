b025eb8bd1cde6b77826e87f40be48db84a34fd8
Nicolò Carandini <n.carandini@outlook.com>
2017-03-10 15:18:45 +0100

    Create Soulution

---
+++ TestMessagePump/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestMessagePump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
